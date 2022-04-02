e010952d2c1bed95308c6306fabaceb06d9e2498
王禹诺 <1216129807@qq.com>
2022-04-02 22:17:50 +0800

    resource redirection

--- src/MainApp/GlobalData.java
+++ src/MainApp/GlobalData.java
@@ -1,12 +1,7 @@
 package MainApp;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
